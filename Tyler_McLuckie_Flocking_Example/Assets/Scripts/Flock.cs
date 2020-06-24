211d320c58e51a0de60ac9046e4aa9fdeec105cf
TylerMcLuckie <37054220+TylerMcLuckie@users.noreply.github.com>
2020-06-24 23:20:46 +1200

    First Commit

---
+++ Tyler_McLuckie_Flocking_Example/Assets/Scripts/Flock.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager myManager;
+    float speed;
+    bool turning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed,
+                             myManager.maxSpeed);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //determine the bounding box of the manager cube
+        Bounds b = new Bounds(myManager.transform.position, myManager.flyLimits * 2);
+
+        //if butterfly is outside the bounds of the cube or (||) about to hit something, turn around
+        //Made the above comment a if statement
+        //if butterfly is outside the bounds of the cube then start turning around 
+        RaycastHit hit = new RaycastHit(); //new raycast working out the 
+        Vector3 direction = Vector3.zero;
+
+
+        if (!b.Contains(transform.position))
+        {
+            turning = true;
+            direction = myManager.transform.position - transform.position;
+        }
+        else if (Physics.Raycast(transform.position, this.transform.forward * 30, out hit))
+
+        {
+            turning = true;
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+            //Reflection vector
+            //Debug.DrawRay(transform.position, this.transform.forward * 30, Color.red);
+        }
+        else
+            turning = false;
+
+        if (turning)
+        {
+            //turn towards the centre of the manager cube
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    myManager.rotationSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+
+
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(myManager.minSpeed,
+                                 myManager.maxSpeed);
+            if (Random.Range(0, 100) < 20)
+                ApplyRules();
+        }
+
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = myManager.allCube;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (nDistance <= myManager.neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if (nDistance < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+
+                }
+
+            }
+
+        }
+
+        if (groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (myManager.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                      Quaternion.LookRotation(direction),
+                                                      myManager.rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Tyler_McLuckie_Flocking_Example/Assets/Scripts/FlockManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int numCube = 20;
+    public GameObject[] allCube;
+    public Vector3 flyLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos;
+
+    [Header("Butterfly Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allCube = new GameObject[numCube];
+        for (int i = 0; i < numCube; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-flyLimits.x, flyLimits.x),
+                                                                Random.Range(-flyLimits.y, flyLimits.y),
+                                                                Random.Range(-flyLimits.z, flyLimits.z));
+            allCube[i] = (GameObject)Instantiate(cubePrefab, pos, Quaternion.identity);
+            allCube[i].GetComponent<Flock>().myManager = this;
+        }
+        goalPos = this.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+            goalPos = this.transform.position + new Vector3(Random.Range(-flyLimits.x, flyLimits.x),
+                                                           Random.Range(-flyLimits.y, flyLimits.y),
+                                                           Random.Range(-flyLimits.z, flyLimits.z));
+
+    }
+}
+
+
